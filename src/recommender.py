192a609603901b2c92aadf6ac0444f8c51842e9a
hieupham <hieupham@maxflowtech.com>
2025-09-10 15:50:12 +0700

    add: add app.py to run streamlit

--- src/recommender.py
+++ src/recommender.py
@@ -1,4 +1,4 @@
-from lanchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA
 from langchain_groq import ChatGroq
 from src.prompt_template import get_anime_prompt
 
@@ -7,6 +7,13 @@ class AnimeRecommender:
         self.llm = ChatGroq(api_key=api_key,model=model_name,temperature=0)
         self.prompt = get_anime_prompt()
 
+        """
+        Creates a chain that retrieves relevant documents ("context") using the retriever,
+        then passes both the context and your query (mapped to "question") to the prompt template.
+        When you call self.qa_chain({"query": query}), the chain automatically:
+            - Uses the retriever to get the context for your query.
+            - Fills the prompt template with the retrieved context and your query.
+        """
         self.qa_chain = RetrievalQA.from_chain_type(
             llm=self.llm,
             chain_type="stuff",
